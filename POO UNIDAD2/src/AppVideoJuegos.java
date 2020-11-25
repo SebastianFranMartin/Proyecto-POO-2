de3932781f379741f86a244023815ccd07a6ed83
Alvaro-VV <65559242+Alvaro-VV@users.noreply.github.com>
2020-11-25 10:12:08 -0300

    Agregado del interfaz en Appvj
    se creó una variable pequeña que almacena la opción del usuario

--- POO UNIDAD2/src/AppVideoJuegos.java
+++ POO UNIDAD2/src/AppVideoJuegos.java
@@ -2,10 +2,21 @@ import java.util.Scanner;
 
 public class AppVideoJuegos
 {
-    private Scanner scan;
+    private static Scanner scan;
 
     public static void main(String[] ags)
     {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("///// MENÚ DE OPERACIONES /////");
+        System.out.println("1. CREAR TRABAJADOR\n" +
+                "2. BUSCAR TRABAJADOR\n" +
+                "3. AUMENTAR SUELDO TRABAJADOR\n" +
+                "4. CAMBIAR SUELDO TRABAJADOR\n" +
+                "5. VER TRABAJADORES\n" +
+                "6. VER LA ACTIVIDAD DE UN TRABAJADOR EN PARTICULAR\n" +
+                "7. SALIR\n");
+        System.out.println("seleccione una opción escribiendo el número de la operación a realizar");
+        int seleccion = scan.nextInt();
 
     }
 }
