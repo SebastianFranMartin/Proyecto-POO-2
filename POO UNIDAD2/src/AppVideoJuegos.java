02e75e9d48092b7456683de528457e8fc7fc7fc5
SebastianFranMartin <65563860+SebastianFranMartin@users.noreply.github.com>
2020-11-25 09:48:50 -0300

    Estructura básica para las clases AppVideojuegos y Empresa

--- POO UNIDAD2/src/AppVideoJuegos.java
+++ POO UNIDAD2/src/AppVideoJuegos.java
@@ -1,2 +1,11 @@
-public class AppVideoJuegos {
+import java.util.Scanner;
+
+public class AppVideoJuegos
+{
+    private Scanner scan;
+
+    public static void main(String[] ags)
+    {
+
+    }
 }
