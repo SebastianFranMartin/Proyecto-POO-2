854ad162750773e9d86ce644cde21127e287a021
SebastianFranMartin <65563860+SebastianFranMartin@users.noreply.github.com>
2020-11-25 10:29:21 -0300

    Añadidos trabajadores y departamentos
    1) El constructor Empresa añade 5 trabajadores y 2 departamentos.
    2) Añadido un constructor para Trabajador, solicita rut, nombre, sueldo y juego.

--- POO UNIDAD2/src/Empresa.java
+++ POO UNIDAD2/src/Empresa.java
@@ -15,6 +15,23 @@ public class Empresa
         direccion = "";
         departamentos = new Departamento[2];
         listaDeTrabajadores = new ArrayList<Trabajador>();
+
+        departamentos[0] = new Departamento("Producción", (short)4, "Encargado de la creación de videojuegos como tal.");
+        departamentos[1] = new Departamento("Publicidad y Marketing", (short)3, "Encargado de la promoción de los juegos de la empresa.");
+
+        listaDeTrabajadores.add(new Trabajador("17.392.221-0", "Antonio",   250000, "Juego 1"));
+        listaDeTrabajadores.add(new Trabajador("16.992.409-2", "Camila",    300000, "Juego 2"));
+        listaDeTrabajadores.add(new Trabajador("15.398.923-7", "Horacio",   350000, "Juego 1"));
+        listaDeTrabajadores.add(new Trabajador("14.419.656-K", "Esmeralda", 400000, "Juego 2"));
+        listaDeTrabajadores.add(new Trabajador("13.033.461-1", "Waldo",     450000, "Juego 1"));
+
+            /*
+            for(byte i = 0;i<5;i++)
+            {
+                Trabajador a = listaDeTrabajadores.get(i);
+                System.out.println(a.rut + ", " + a.nombre + ", " + a.sueldo + ", " + a.juego);
+            }
+            */
     }
 
     //GETTERS
