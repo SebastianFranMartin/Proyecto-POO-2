956aeb250a9ecd775b675c526224f2a614849ed1
SebastianFranMartin <65563860+SebastianFranMartin@users.noreply.github.com>
2020-11-29 19:00:37 -0300

    Constructores clases hijas
    Desarrollador, Diseniador y Publicista ahora tienen constructores que inicializan su actividad.

--- POO UNIDAD2/src/Diseniador.java
+++ POO UNIDAD2/src/Diseniador.java
@@ -1,2 +1,9 @@
-public class Diseniador extends Trabajador{
+public class Diseniador extends Trabajador
+{
+    public Diseniador(String rut, String nombre, int sueldo, String juego, Departamento departamento)
+    {
+        super(rut, nombre, sueldo, juego, departamento);
+
+        actividadActual = "Creando sprites del videojuego.";
+    }
 }
--- POO UNIDAD2/src/Publicista.java
+++ POO UNIDAD2/src/Publicista.java
@@ -1,6 +1,9 @@
-public class Publicista extends Trabajador{
+public class Publicista extends Trabajador
+{
+    public Publicista(String rut, String nombre, int sueldo, String juego, Departamento departamento)
+    {
+        super(rut, nombre, sueldo, juego, departamento);
 
-    public Publicista(){
+        actividadActual = "Preparándose para la fecha de lanzamiento del videojuego.";
     }
-
 }
